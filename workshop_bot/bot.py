496d88c010ecc462b29d613933a8e081712e6d60
Will Russell <will@will-russell.com>
2020-03-18 02:36:38 +0000

    Add on_message

--- workshop_bot/bot.py
+++ workshop_bot/bot.py
@@ -5,9 +5,17 @@ from . import config
 
 bot = commands.Bot(command_prefix='?')
 
+def main():
+    bot.run(config.creds['token'])
+
 @bot.event
 async def on_ready():
     print('Ready!')
 
-def main():
-    bot.run(config.creds['token'])
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    await bot.process_commands(message)
+    await message.channel.send("No, fuck off")
