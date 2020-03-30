6004a06f80eba58565f05afeb7deb5e90e6e7b1e
Will Russell <will@will-russell.com>
2020-03-30 08:25:27 +0100

    Use strftime to remove long second decimal

--- calendar_bot/scheduler.py
+++ calendar_bot/scheduler.py
@@ -1,6 +1,7 @@
 import discord
 import asyncio
 import datetime
+import dateutil.parser
 import pytz
 from . import config
 from . import calendar
@@ -11,17 +12,21 @@ from calendar_bot.logging import logger
 
 async def check_schedule():
     await bot.wait_until_ready()
-    # Repeat every 60 seconds
-    utc = pytz.UTC
     while True:
         now = datetime.datetime.now()
     
         cal_event = calendar.get_next_event(now)
-        start_time = cal_event.start.replace(tzinfo=utc)
-        if now.replace(tzinfo=utc) == (start_time - datetime.timedelta(minutes=10)).replace(tzinfo=utc):
+        current_time = dateutil.parser.parse(now.strftime("%Y-%m-%d %H:%M:%S"))
+        start_time = cal_event.start
+        announcement_time = (start_time - datetime.timedelta(minutes=10)).strftime("%Y-%m-%d %H:%M:%S")
+        
+        logger.debug(f'{current_time} : Now')
+        logger.debug(f'{announcement_time} : Message time')
+        
+        if current_time == announcement_time:
             await send_announcement(cal_event)
             logger.debug("Send announcement")
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.2)
 
 async def send_announcement(cal_event):
 
