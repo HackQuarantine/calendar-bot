eb1e648ea99221ada15a512bfcd21fc1cb9c8c87
Will Russell <will@will-russell.com>
2020-04-01 05:16:03 +0100

    Import setup

--- calendar_bot/stream.py
+++ calendar_bot/stream.py
@@ -1,7 +1,9 @@
 import twitch
+import calendar_bot.setup
+
 
 def change_stream_title(title):
     stream_title = f"{title} - Hack Quarantine powered by GitHub"
 
 def set_default_title():
-    pass
+    stream_title = "Hack Quarantine - Online Hackathon!"
