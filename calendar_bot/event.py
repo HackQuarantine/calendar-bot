b157623b7d4db0e4206d8c5b9ce1302cb1adbcbc
Will Russell <will@will-russell.com>
2020-04-05 17:11:34 +0100

    Add ceremony announcement

--- calendar_bot/event.py
+++ calendar_bot/event.py
@@ -26,6 +26,8 @@ class Event:
                 return f"{self.get_message_start()} We have {self.organiser} giving a {self.type} on '{self.title}' in 10 minutes on `Twitch.tv`! Head over and check it out!\nhttps://hackquarantine.com/stream"
             else: 
                 return f"{self.get_message_start()} We have a {self.type} on '{self.title}' in 10 minutes on Twitch.tv!\n\nHead over and check it out!\n\nhttps://hackquarantine.com/stream"
+        elif self.type == "ceremony":
+            return f"The {self.title} is starting in 10 minutes on`Twitch.tv`!\nHead over to https://hackquarantine.com/stream to join in!"
 
     def get_discord_nick(self):
         self.organiser_discord_name = bot.get_guild(config.creds['guild_id']).get_member(int(self.organiser_id))
